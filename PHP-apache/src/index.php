6a32f1c340727ce4058b4628019748bd3bf028be
YACRUZ <yamatsukino@gmail.com>
2023-08-30 10:29:27 -0600

    Add PHP-APACHE

--- PHP-apache/src/index.php
+++ PHP-apache/src/index.php
@@ -1,3 +1,25 @@
-<?php
-echo "Hola mundo";
-?>
+<html>
+    <body>
+        <?php
+            for ($i = 0; $i < 10; $i++) {
+                echo "Hello World! <br>";
+            }
+            try {
+                $url = "pgsql:host=172.17.0.3;port=5432;dbname=mydb;";
+                // make a database connection
+                $pdo = new PDO($url, "postgres", "postgres", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            
+                if ($pdo) {
+                    echo "Connected to the $db database successfully!";
+                }
+            } catch (PDOException $e) {
+                die($e->getMessage());
+            } finally {
+                if ($pdo) {
+                    $pdo = null;
+                }
+            }
+            phpinfo();
+        ?>
+    </body>
+</html>
