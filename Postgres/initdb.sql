a36b8793661df6dbd13c2838c65b2c665e9a973e
Loken <rodrigomencias08@gmail.com>
2023-09-05 12:17:13 -0600

    base del proyecto

--- Postgres/initdb.sql
+++ Postgres/initdb.sql
@@ -3,12 +3,12 @@ CREATE DATABASE mydb;
 GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
 ALTER DATABASE mydb OWNER TO myuser;
 
-\connect mydb
-create table empleado
-(
-    clave integer NOT NULL,
-    nombre character varying,
-    direccion character varying,
-    telefeno character varying
+connect mydb
+
+CREATE TABLE empleado (
+    clave SERIAL PRIMARY KEY,
+    nombre VARCHAR(255),
+    direccion VARCHAR(255),
+    telefono VARCHAR(15)
 );
 ALTER TABLE empleado OWNER TO myuser;
